f46a0cc4e0719be27f3bdb0058e240e4b43b012e
MJ_Shelton <mjshelton12@comcast.net>
2023-04-17 22:02:00 -0700

    Partially built letter display functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 const letters = document.querySelectorAll('.letter')
+const word1Box = document.getElementById('word-1-box')
 
-console.log("connected")
+const word1 = ["", "", "", "", ""]
+const word2 = []
+const word3 = []
+const word4 = []
+const word5 = []
+const word6 = []
+
+var letterEntered = document.createElement("p");
+letterEntered.textContent = word1[0]
+word1Box.appendChild(letterEntered)
+
+function fillBoxes() {
+    for(let i=0; i<5; i++){
+        var letterBox = document.createElement("div")
+        var letterEntered = document.createElement("p");
+        letterEntered.textContent = word1[i]
+        letterBox.classList = "letter-box"
+        letterBox.appendChild(letterEntered)
+        word1Box.appendChild(letterBox)
+    }
+}
 
 function letterPress(letter){
-    console.log("button", letter)
+    if(word1.length < 10){
+        console.log(letter)
+        word1.unshift(letter)
+        console.log(word1)
+    }
 }
 
 letters.forEach(letter => {
 
     letter.addEventListener('click', event => {
-         console.log( event.target.innerHTML.trim() );
+         letterPress( event.target.innerHTML.trim() );
     });
  
  });
+
+ fillBoxes()
