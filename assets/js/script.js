f6ff04fc4bf94911c12c9ff640f0ed63d98ddefc
MJ_Shelton <mjshelton12@comcast.net>
2023-04-22 23:23:51 -0700

    All letters appended to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-const letters = document.querySelectorAll('.letter')
 const word1Box = document.getElementById('word-1-box')
+const deleteButton = document.getElementById('delete')
+const keyRow1 = document.getElementById('keys-row-1')
+const keyRow2 = document.getElementById('keys-row-2')
+const keyRow3 = document.getElementById('keys-row-3')
 
 const word1 = []
 const word2 = []
@@ -8,9 +11,37 @@ const word4 = []
 const word5 = []
 const word6 = []
 
+const row1 = "QWERTYUIOP"
+const row2 = "ASDFGHJKL"
+const row3 = "ZXCVBNM"
+
 var letterEntered = document.createElement("p");
 letterEntered.textContent = word1[0]
 word1Box.appendChild(letterEntered)
+
+function buildLetters() {
+    for(let i=0; i<row1.length; i++){
+        let key = document.createElement("button")
+        key.textContent = row1[i]
+        key.classList = "letter"
+        keyRow1.appendChild(key)
+    }
+    for(let i=0; i<row2.length; i++){
+        let key = document.createElement("button")
+        key.textContent = row2[i]
+        key.classList = "letter"
+        keyRow2.appendChild(key)
+    }
+    for(let i=0; i<row2.length; i++){
+        let key = document.createElement("button")
+        key.textContent = row3[i]
+        key.classList = "letter"
+        keyRow3.appendChild(key)
+    }
+}
+
+buildLetters()
+const letters = document.querySelectorAll('.letter')
 
 function fillBoxes() {
     word1Box.innerHTML = ""
@@ -33,6 +64,11 @@ function letterPress(letter){
     fillBoxes()
 }
 
+function deleteButtonPress(){
+    word1.pop()
+    fillBoxes()
+}
+
 letters.forEach(letter => {
 
     letter.addEventListener('click', event => {
@@ -40,5 +76,8 @@ letters.forEach(letter => {
     });
  
  });
+
+ deleteButton.addEventListener("click", event => {
+    deleteButtonPress()})
 
  fillBoxes()
